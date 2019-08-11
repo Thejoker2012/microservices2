ff67007d5f4f488ae5123a8309799c70a00e1c5d
Iago <iagofalcom@hotmail.com>
2019-08-11 19:20:45 -0300

    Implementando Configuração de Autenticação Utilizando WebSecurityConfigurerAdapter

---
+++ src/main/java/br/com/iago/learning/microservices/config/WebSecurityConfig.java
@@ -0,0 +1,64 @@
+package br.com.iago.learning.microservices.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    protected void configure(HttpSecurity http) throws Exception {
+
+        //Cross origin resources sharing
+        http.cors().and()
+                //Starts authorizing configurations.
+                .authorizeRequests()
+                //ignoring the guest's urls...
+                .antMatchers("/resources/**", "/error", "/service/**").permitAll()
+                //authentication all remaining URLs.
+                .anyRequest().authenticated()
+                .and()
+                .logout().permitAll()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/service/logout", "POST"))
+                //login form
+                .and()
+                .formLogin().loginPage("/service/login").and()
+                //enable basic header authentication
+                .httpBasic().and()
+                //cross-side request forgery
+                .csrf().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
+}
